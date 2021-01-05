11bd36dc3ecc1d189398b74e72015956dae5f2cb
Jeongyeob Hong <jhong2@macalester.edu>
2021-01-04 23:58:42 -0600

    1/4 Commit

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 print("Hello World")
 
 print("Is this all right?")
+
+previous = 0
+current = 1
+i = 0
+while i < 20:
+    print(current)
+    temp = previous
+    previous = current
+    current = temp + current
+    i += 1
