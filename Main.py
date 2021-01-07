9e1b3d595435e3af1a6f4af3e1d65e32773672b0
Jeongyeob Hong <jhong2@macalester.edu>
2021-01-06 23:32:04 -0600

    1/6 Commit

--- Main.py
+++ Main.py
@@ -29,3 +29,17 @@ print_grade(40, 45)
 print_grade(20, 35)
 print_grade(30, 32)
 print_grade(50, 45)
+
+#Prize
+year = 1988
+total = 50000000
+APT = 1100000000
+INTEREST = 0.12
+while year < 2016:
+    total = total * (1 + INTEREST)
+    year += 1
+
+if total > APT:
+    print("%d원 차이로 동일 아저씨의 말씀이 맞습니다" % (total - APT))
+else:
+    print("%d원 차이로 미란 아주머니의 말씀이 맞습니다" % (APT - total))
